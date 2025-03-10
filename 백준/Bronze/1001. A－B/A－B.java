d6b0aad9e9ef225d327a96545a3044355fec8bf1
vyjsjs <59689357+vyjsjs@users.noreply.github.com>
2025-03-10 21:21:31 +0900

    [Bronze V] Title: A-B, Time: 176 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,18 @@
+/*
+작성일자: 25.03.10.(월)
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args){
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        System.out.println(a-b);
+    }
+}
